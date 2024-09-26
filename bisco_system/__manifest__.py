0cd68f80f1216c824f71f978262999b4ab8ee129
Jagdish <jagdishranjith>
2024-09-26 14:21:45 +0530

    new module added bisco_sys_app

---
+++ bisco_system/__manifest__.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Bisco sys app',
+    'version': '1.0',
+    'category': 'Sale',
+    'summary': 'Upgrade the base fields attributes',
+    'depends': ['sale'],
+
+    'data': [
+        # 'views/views.xml',
+    ],
+
+}
